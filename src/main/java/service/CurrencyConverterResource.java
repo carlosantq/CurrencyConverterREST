e05670b2b08605541648db3a5ea967b6234c8358
Carlos Antonio <carlosantonio.o.n@outlook.com>
2018-10-10 09:24:48 -0300

    Conversão de uma moeda para todas finalizada

--- src/main/java/service/CurrencyConverterResource.java
+++ src/main/java/service/CurrencyConverterResource.java
@@ -8,8 +8,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.ParseException;
@@ -19,12 +20,14 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 @Stateless
 @Path("/converter")
 public class CurrencyConverterResource implements CurrencyConverterInterface{
+	
 	//Atributos utilizados no acesso da API externa
 	public static final String ACCESS_KEY = "7f7012ebf6b30f1cab604e8a4f0737fa";
 	public static final String BASE_URL = "http://apilayer.net/api/";
@@ -36,17 +39,20 @@ public class CurrencyConverterResource implements CurrencyConverterInterface{
 	public static final String STATIC_DOLAR = "USD";
 	static CloseableHttpClient httpClient = HttpClients.createDefault();
 	
+	/**
+	 * Construtor Padrão
+	 */
 	public CurrencyConverterResource() {
-		
 	}
 
 	@GET
 	@Path("{moedaOrigem}/{moedaDestino}/{valor}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String currencyAToB(@PathParam("moedaOrigem") String from, @PathParam("moedaDestino") String to, @PathParam("valor") Double value){
-		Double result = sendLiveRequest(from, to);
+		Double result = convert(requestAPI(), from, to);
 		if (result == null) {
-			return "Not Available";
+			//return "Not Available";
+			return Response.status(Status.BAD_REQUEST).toString();
 		}
 		
 		String json = new JSONObject().put("moedaOrigem", from).put("moedaDestino", to).put("valor", (value * result)).toString();
@@ -58,22 +64,53 @@ public class CurrencyConverterResource implements CurrencyConverterInterface{
 	@Path("{moedaOrigem}/{valor}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String currencyAToAll(@PathParam("moedaOrigem") String from, @PathParam("valor") Double value) throws RemoteException {
-		return "Teste";
+		JSONObject apiJSON = requestAPI();
+		
+		//Tratar exceção do caso NULL na conversão
+		JSONArray jsonArray = new JSONArray();
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "DKK").put("valor", (value * convert(apiJSON, from, "DKK"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "NOK").put("valor", (value * convert(apiJSON, from, "NOK"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "SEK").put("valor", (value * convert(apiJSON, from, "SEK"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "CZK").put("valor", (value * convert(apiJSON, from, "CZK"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "GBP").put("valor", (value * convert(apiJSON, from, "GBP"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "TRY").put("valor", (value * convert(apiJSON, from, "TRY"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "INR").put("valor", (value * convert(apiJSON, from, "INR"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "IDR").put("valor", (value * convert(apiJSON, from, "IDR"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "PKR").put("valor", (value * convert(apiJSON, from, "PKR"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "THB").put("valor", (value * convert(apiJSON, from, "THB"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "USD").put("valor", (value * convert(apiJSON, from, "USD"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "AUD").put("valor", (value * convert(apiJSON, from, "AUD"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "CAD").put("valor", (value * convert(apiJSON, from, "CAD"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "SGD").put("valor", (value * convert(apiJSON, from, "SGD"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "HKD").put("valor", (value * convert(apiJSON, from, "HKD"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "TWD").put("valor", (value * convert(apiJSON, from, "TWD"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "NZD").put("valor", (value * convert(apiJSON, from, "NZD"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "EUR").put("valor", (value * convert(apiJSON, from, "EUR"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "HUF").put("valor", (value * convert(apiJSON, from, "HUF"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "CHF").put("valor", (value * convert(apiJSON, from, "CHF"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "JPY").put("valor", (value * convert(apiJSON, from, "JPY"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "ILS").put("valor", (value * convert(apiJSON, from, "ILS"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "CLP").put("valor", (value * convert(apiJSON, from, "CLP"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "PHP").put("valor", (value * convert(apiJSON, from, "PHP"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "MXN").put("valor", (value * convert(apiJSON, from, "MXN"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "ZAR").put("valor", (value * convert(apiJSON, from, "ZAR"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "BRL").put("valor", (value * convert(apiJSON, from, "BRL"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "MYR").put("valor", (value * convert(apiJSON, from, "MYR"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "RUB").put("valor", (value * convert(apiJSON, from, "RUB"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "KRW").put("valor", (value * convert(apiJSON, from, "KRW"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "CNY").put("valor", (value * convert(apiJSON, from, "CNY"))));
+		jsonArray.put(new JSONObject().put("moedaOrigem", from).put("moedaDestino", "PLN").put("valor", (value * convert(apiJSON, from, "PLN"))));
+		
+		return jsonArray.toString();
+		
 	}
 	
 	/**
-	 * Método auxiliar criado para extrair da API externa o valor de 1 unidade da moeda de origem e da de destino 
-	 * em dólares, para então ser feita a conversão de uma moeda para a outra.
-	 * @param value - valor a ser convertido
-	 * @param from - moeda de origem
-	 * @return o valor convertido de uma moeda para outra ou NULL no caso de requisições com erro da API
+	 * Método encarregado de fazer a requisição para a API do CurrencyLayer para pegar os 
+	 * valores de todas as moedas na unidade DÓLAR.
+	 * @return JSON de resposta da requisição.
 	 */
-	private Double sendLiveRequest(String from, String to) {
-
-		//Variáveis auxiliares
-		Double from2 = 1.0;
-		Double to2 = 1.0;
-		
+	private JSONObject requestAPI() {
 		// Inicializa o objeto HttpGet com a URL para mandar a requisição para a API
 		HttpGet get = new HttpGet(BASE_URL + ENDPOINT + "?access_key=" + ACCESS_KEY);
 		try {
@@ -101,33 +138,9 @@ public class CurrencyConverterResource implements CurrencyConverterInterface{
 				System.exit(0);
 			}
 			
-			
-			//Valor equivalente à 1 dólar na moeda de origem
-			System.out.println("Converting " + STATIC_DOLAR + " in " + from + ": "
-					+ exchangeRates.getJSONObject("quotes").getDouble(STATIC_DOLAR + from));
-			
-			//Divisão para transformar o valor recuperado da API em 1 unidade da moeda de origem
-			from2 = 1 / exchangeRates.getJSONObject("quotes").getDouble(STATIC_DOLAR + from);
-			
-			//Impressão no console 
-			System.out.println("1 unity of " + from + " in USD: " + from2);
-			
-			//Valor equivalente à 1 dólar na moeda de destino
-			System.out.println("Converting " + STATIC_DOLAR + " in " + to + ": "
-					+ exchangeRates.getJSONObject("quotes").getDouble(STATIC_DOLAR + to));
-			
-			//Divisão para transformar o valor recuperado da API em 1 unidade da moeda de destino
-			to2 = 1 / exchangeRates.getJSONObject("quotes").getDouble(STATIC_DOLAR + to);
-			
-			//Impressão no console 
-			System.out.println("1 unity of " + to + " in USD: " + to2);
-			
-			//Impressão no console do resultado da conversão da moeda de origem para a de destino
-			System.out.println(from + " IN " + to + ": " + from2 / to2);
-
 			response.close();
-			return from2 / to2;
-
+			return exchangeRates;
+			
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -137,7 +150,64 @@ public class CurrencyConverterResource implements CurrencyConverterInterface{
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
+			
 		return null;
+	}
+	
+	/**
+	 * Método auxiliar utilizado para validar as moedas da conversão
+	 * @param currency - sigla da moeda.
+	 * @return true caso a moeda seja válida, false caso contrário.
+	 */
+	private boolean validateCurrency(String currency) {
+		
+		String[] currencies = { "DKK", "NOK", "SEK", "CZK", "GBP", "TRY", "INR",
+				"IDR", "PKR", "THB", "USD", "AUD", "CAD", "SGD", "HKD", "TWD", 
+				"NZD", "EUR", "HUF", "CHF", "JPY", "ILS", "CLP", "PHP", "MXN", 
+				"ZAR", "BRL", "MYR", "RUB", "KRW", "CNY", "PLN" };
+		
+		int aux = 0; 
+		
+		//Confere se a moeda utilizada é alguma das suportadas pela aplicação
+		for (int i=0; i<currencies.length; i++) {
+			if (!currency.equals(currencies[i])) {
+				aux++;
+			}
+		}
+		if (aux == 32) {
+			return false;
+		}else {
+			return true;
+		}
+	}
+	
+	/**
+	 * Método auxiliar criado para extrair do JSON da API externa o valor de 1 unidade da moeda de origem e da de destino 
+	 * em dólares, para então ser feita a conversão de uma moeda para a outra.
+	 * @param exchangeRates JSON resposta da requisição do CurrencyLayer
+	 * @param from - moeda de origem
+	 * @param to - moeda de destino
+	 * @return o valor convertido de uma moeda para outra ou NULL no caso de moeda ser inválida.
+	 */
+	private Double convert(JSONObject exchangeRates, String from, String to) {
+
+		//Variáveis auxiliares
+		Double from2 = 1.0;
+		Double to2 = 1.0;
+			
+		if (validateCurrency(from) == false) {
+			return null;
+		}else if (validateCurrency(to) == false) {
+			return null;
+		}else {
+			//Divisão para transformar o valor recuperado da API em 1 unidade da moeda de origem
+			from2 = 1 / exchangeRates.getJSONObject("quotes").getDouble(STATIC_DOLAR + from);
+			
+			//Divisão para transformar o valor recuperado da API em 1 unidade da moeda de destino
+			to2 = 1 / exchangeRates.getJSONObject("quotes").getDouble(STATIC_DOLAR + to);
+			
+			return from2 / to2;
+		}
 
 	}
 	
