5cb0a5756cc764bb7ecc71626cf6f769668c7c4d
Carlos Antonio <carlosantonio.o.n@outlook.com>
2018-10-10 22:10:19 -0300

    Imports desnecessários

--- src/main/java/service/CurrencyConverterResource.java
+++ src/main/java/service/CurrencyConverterResource.java
@@ -1,7 +1,6 @@
 package service;
 
 import java.io.IOException;
-import java.rmi.RemoteException;
 
 import javax.ejb.Stateless;
 import javax.ws.rs.GET;
@@ -9,8 +8,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.ParseException;
